fcfe78e78ffd6523278944abdb1e881d27ba1442
Tim Rogers <tim@gocardless.com>
2015-06-03 13:57:16 +0100

    Add blank option, rename field

--- app/components/prospect-form/prospect-form.fr.js
+++ app/components/prospect-form/prospect-form.fr.js
@@ -7,10 +7,11 @@ export default class ProspectFormFr extends React.Component {
   render() {
     return (
       <Translation locales='fr'>
-        <label className='label label--stacked' htmlFor='prospect_metadata_payments_per_month'>
+        <label className='label label--stacked' htmlFor='prospect_metadata_number_of_payments'>
             Combien de paiements voulez-vouz prendre chaque mois?
           </label>
-        <select className='select input--stacked' id='prospect_metadata_payments_per_month' name='prospect[metadata][payments_per_month]'>
+        <select className='select input--stacked' id='prospect_metadata_number_of_payments' name='prospect[metadata][number_of_payments]'>
+          <option></option>
           <option value='0-50'>0-50</option>
           <option value='50+'>50+</option>
         </select>
